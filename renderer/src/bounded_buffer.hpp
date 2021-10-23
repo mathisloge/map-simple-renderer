61c0c2ca1e8caa980a86d45c4a33c90ee178604b
Mathis Logemann <mathisloge@gmail.com>
2021-10-23 18:22:36 +0200

    drop some boost deps

--- renderer/src/bounded_buffer.hpp
+++ renderer/src/bounded_buffer.hpp
@@ -1,10 +1,8 @@
 #pragma once
-#include <boost/bind.hpp>
-#include <boost/call_traits.hpp>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
 #include <boost/circular_buffer.hpp>
-#include <boost/thread/condition.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/thread/thread.hpp>
 
 template <class T>
 class bounded_buffer
@@ -13,36 +11,34 @@ class bounded_buffer
     typedef boost::circular_buffer<T> container_type;
     typedef typename container_type::size_type size_type;
     typedef typename container_type::value_type value_type;
-    typedef typename boost::call_traits<value_type>::param_type param_type;
 
     explicit bounded_buffer(size_type capacity)
         : m_unread(0)
         , m_container(capacity)
     {}
+    bounded_buffer(const bounded_buffer &) = delete;
+    bounded_buffer &operator=(const bounded_buffer) = delete;
 
-    void push_front(typename boost::call_traits<value_type>::param_type item)
-    { // `param_type` represents the "best" way to pass a parameter of type `value_type` to a method.
-
-        boost::mutex::scoped_lock lock(m_mutex);
-        m_not_full.wait(lock, boost::bind(&bounded_buffer<value_type>::is_not_full, this));
-        m_container.push_front(item);
-        ++m_unread;
-        lock.unlock();
-        m_not_empty.notify_one();
+    void push_front(T &&item)
+    {
+        {
+            std::unique_lock<std::mutex> lk{mtx_};
+            not_full_.wait(lk, std::bind(&bounded_buffer<T>::is_not_full, this));
+            m_container.push_front(std::forward<T>(item));
+            ++m_unread;
+        }
+        not_empty_.notify_one();
     }
 
     void pop_back(value_type *pItem)
     {
-        boost::mutex::scoped_lock lock(m_mutex);
-        m_not_empty.wait(lock, boost::bind(&bounded_buffer<value_type>::is_not_empty, this));
-        *pItem = m_container[--m_unread];
-        lock.unlock();
-        m_not_full.notify_one();
+        {
+            std::unique_lock<std::mutex> lk{mtx_};
+            not_empty_.wait(lk, std::bind(&bounded_buffer<T>::is_not_empty, this));
+            *pItem = m_container[--m_unread];
+        }
+        not_full_.notify_one();
     }
-
-  private:
-    bounded_buffer(const bounded_buffer &);            // Disabled copy constructor.
-    bounded_buffer &operator=(const bounded_buffer &); // Disabled assign operator.
 
     bool is_not_empty() const
     {
@@ -55,7 +51,7 @@ class bounded_buffer
 
     size_type m_unread;
     container_type m_container;
-    boost::mutex m_mutex;
-    boost::condition m_not_empty;
-    boost::condition m_not_full;
-}; 
+    std::mutex mtx_;
+    std::condition_variable not_empty_;
+    std::condition_variable not_full_;
+};
--- renderer/src/main.cpp
+++ renderer/src/main.cpp
@@ -1,11 +1,12 @@
+#include <chrono>
 #include <filesystem>
-#include <boost/timer/timer.hpp>
 #include <mapnik/agg_renderer.hpp>
 #include <mapnik/color_factory.hpp>
 #include <mapnik/datasource_cache.hpp>
 #include <mapnik/font_engine_freetype.hpp>
 #include <mapnik/load_map.hpp>
 #include <mapnik/map.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 #include "bounded_buffer.hpp"
 #include "version.hpp"
@@ -48,12 +49,13 @@ struct RenderEntry
 using Queue = bounded_buffer<RenderEntry>;
 class RenderThread final
 {
+    using MyRenderer = mapnik::agg_renderer<mapnik::image_rgba8>;
+
   public:
     RenderThread(int renderer_id, const fs::path &xml_file, Queue &queue, int w, int h)
         : renderer_id_{renderer_id}
         , map_file_{xml_file}
         , queue_{queue}
-        , map_{w, h}
 
     {
         render_thread_ = std::thread{std::bind(&RenderThread::render_thread, this)};
@@ -67,9 +69,12 @@ class RenderThread final
 
     void render_thread()
     {
-        spdlog::info("setup renderer {}", renderer_id_);
-        mapnik::load_map(map_, map_file_.string(), true);
-        spdlog::info("renderer {} ready", renderer_id_);
+        auto logger = spdlog::get("console");
+        logger->info("setup renderer {}", renderer_id_);
+        mapnik::Map map;
+        mapnik::load_map(map, map_file_.string(), true);
+        logger->info("renderer {} ready", renderer_id_);
+
         while (true)
         {
             RenderEntry e;
@@ -79,21 +84,21 @@ class RenderThread final
             {
                 try
                 {
-                    render_tile(e.tile_path, e.x, e.y, e.z);
+                    render_tile(map, e.tile_path, e.x, e.y, e.z);
                     retry = false;
                 }
                 catch (const std::exception &ex)
                 {
-                    spdlog::error("Error while rendering tile {} {} {}: {}", e.z, e.x, e.y, ex.what());
+                    logger->error("Error while rendering tile {} {} {}: {}", e.z, e.x, e.y, ex.what());
                 }
             }
         }
-        spdlog::info("renderer {} finished", renderer_id_);
+        logger->info("renderer {} finished", renderer_id_);
     }
 
-    void render_tile(const fs::path &tile_uri, int x, int y, int z)
+    void render_tile(mapnik::Map &map, const fs::path &tile_uri, int x, int y, int z)
     {
-        boost::timer::cpu_timer t{};
+        auto logger = spdlog::get("console");
         const std::pair<int, int> p0 = {x, y + 1};
         const std::pair<int, int> p1 = {x + 1, y};
 
@@ -102,36 +107,35 @@ class RenderThread final
         double y0 = tiley2lat(p0.second, z);
         double x1 = tilex2long(p1.first, z);
         double y1 = tiley2lat(p1.second, z);
-
-        spdlog::info("renderer {}: bb({} {} {}) {} {} {} {}", renderer_id_, z, x, y, x0, y0, x1, y1);
-
-        mapnik::proj_transform proj_tr(std::string{"epsg:4326"}, map_.srs());
-        proj_tr.forward(x0, y0, zd);
-        proj_tr.forward(x1, y1, zd);
+        logger->info("renderer {}: bb({} {} {}) {} {} {} {}", renderer_id_, z, x, y, x0, y0, x1, y1);
+        {
+            const mapnik::proj_transform proj_tr{std::string{"epsg:4326"}, map.srs()};
+            proj_tr.forward(x0, y0, zd);
+            proj_tr.forward(x1, y1, zd);
+        }
 
         const mapnik::box2d<double> bbox{x0, y0, x1, y1};
-        map_.zoom_to_box(bbox);
-        if (map_.buffer_size() < 128)
-            map_.set_buffer_size(128);
-
-        mapnik::image_rgba8 buf(map_.width(), map_.height());
-        mapnik::agg_renderer<mapnik::image_rgba8> ren{map_, buf};
-        ren.apply();
-        save_to_file(buf, tile_uri.string(), "png");
-        t.stop();
-        spdlog::info("renderer {}: processed {} {} {} in {}",
+        map.zoom_to_box(bbox);
+        if (map.buffer_size() < 128)
+            map.set_buffer_size(128);
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        mapnik::image_rgba8 buf(map.width(), map.height());
+        {
+            mapnik::agg_renderer<mapnik::image_rgba8> ren{map, buf};
+            ren.apply();
+        }
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        logger->info("renderer {} applied: {}",
                      renderer_id_,
-                     z,
-                     x,
-                     y,
-                     t.format(4, "%ws wall, %us user + %ss system = %ts CPU (%p%)"));
+                     std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
+        save_to_file(buf, tile_uri.string(), "png");
+        logger->info("renderer {}: processed {} {} {}", renderer_id_, z, x, y);
     }
 
   private:
     const int renderer_id_;
     const fs::path map_file_;
     Queue &queue_;
-    mapnik::Map map_;
     std::thread render_thread_;
 };
 struct BboxRenderJob
@@ -152,7 +156,7 @@ class TileRenderer final
         : tile_dir_{tile_dir}
         , queue{num_threads * 2}
     {
-        spdlog::info("creating {} render threads", num_threads);
+        spdlog::get("console")->info("creating {} render threads", num_threads);
         for (int i = 0; i < num_threads; i++)
             renderers.emplace_back(std::make_unique<RenderThread>(i, xml_file, queue, 256, 256));
     }
@@ -171,16 +175,17 @@ class TileRenderer final
     {
         for (const auto &job : render_jobs_)
             process(job);
-        spdlog::info("All queued.");
+        spdlog::get("console")->info("All queued.");
     }
 
   private:
     void process(const BboxRenderJob &job)
     {
+        auto logger = spdlog::get("console");
         if (!fs::exists(tile_dir_))
             fs::create_directory(tile_dir_);
 
-        spdlog::info("start rendering processing {}-{}", job.min_zoom, job.max_zoom);
+        logger->info("start rendering processing {}-{}", job.min_zoom, job.max_zoom);
         for (int z = job.min_zoom; z <= job.max_zoom; z++)
         {
             const double zpow = std::pow(2, z);
@@ -207,11 +212,11 @@ class TileRenderer final
                                             (std::to_string(y) + ".png")};
                     if (!fs::exists(tile_uri))
                     {
-                        spdlog::info("adding tile {} {} {}", z, x, y);
+                        logger->debug("adding tile {} {} {}", z, x, y);
                         queue.push_front(RenderEntry{tile_uri, x, y, z});
                     }
                     else
-                        spdlog::info("skipping tile {} {} {}", z, x, y);
+                        logger->debug("skipping tile {} {} {}", z, x, y);
                 }
             }
         }
@@ -220,11 +225,13 @@ class TileRenderer final
 
 int main(int argc, char const *argv[])
 {
+    auto console = spdlog::stdout_color_mt("console");
+    console->set_pattern("[con](%t) [%^%l%$](%H:%M:%S.%e) %v");
     const fs::path xml_file = "D:/dev/openstreetmap-carto/mapnik.xml";
     const fs::path out_dir = "D:/dev/map-simple-renderer/build/windows-64-default-release/server/websrc/tiles";
-    mapnik::datasource_cache::instance().register_datasources(MAPNIK_PLUGINS_DIR);
+    mapnik::datasource_cache::instance().register_datasources(renderer::kMAPNIK_PLUGINS_DIR);
     mapnik::freetype_engine::register_fonts("./fonts", true);
-    constexpr int kNumThreads = 9;
+    constexpr int kNumThreads = 8;
 
     TileRenderer renderer{xml_file, out_dir, kNumThreads};
     const mapnik::box2d<double> world_bbox{-180.0, -90.0, 180.0, 90.0};
